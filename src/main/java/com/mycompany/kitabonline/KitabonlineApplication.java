b16a1699052e885119969e9f7ef38fbdf933f0b6
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-10 23:10:17 +0400

    Initial commit

---
+++ src/main/java/com/mycompany/kitabonline/KitabonlineApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.kitabonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitabonlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitabonlineApplication.class, args);
+    }
+
+}
