1092d589b234ab0179435ce688bd084e22bf6612
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-10 23:09:32 +0400

    Initial commit

---
+++ src/test/java/com/mycompany/kitabonline/KitabonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.kitabonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitabonlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
