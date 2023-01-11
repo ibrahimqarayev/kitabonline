d22ed3b95c3f5a314f13909ee5eaccac829273a9
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-11 22:53:56 +0400

    Book entity

--- src/main/java/com/mycompany/kitabonline/entity/Book.java
+++ src/main/java/com/mycompany/kitabonline/entity/Book.java
@@ -1,4 +1,25 @@
 package com.mycompany.kitabonline.entity;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "book")
+@Getter
+@NoArgsConstructor
+@Builder
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String authorName;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
 }
