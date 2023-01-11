80c5a195ed9cba4614db398ab62f0265844296fa
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-11 22:55:44 +0400

    Category entity

--- src/main/java/com/mycompany/kitabonline/entity/Category.java
+++ src/main/java/com/mycompany/kitabonline/entity/Category.java
@@ -1,4 +1,19 @@
 package com.mycompany.kitabonline.entity;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "category")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Book> books;
+
 }
