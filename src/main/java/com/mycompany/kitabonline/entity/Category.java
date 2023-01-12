6d614a522ebcaff622593df52b97edd2584383b3
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-12 12:25:17 +0400

    Category entity

--- src/main/java/com/mycompany/kitabonline/entity/Category.java
+++ src/main/java/com/mycompany/kitabonline/entity/Category.java
@@ -1,18 +1,23 @@
 package com.mycompany.kitabonline.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @Table(name = "category")
-public class Category {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+@NoArgsConstructor
+@Builder
+@Data
+public class Category extends BaseEntity {
 
     private String name;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "category")
     private List<Book> books;
 
