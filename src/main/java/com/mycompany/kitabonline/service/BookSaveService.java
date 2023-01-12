a844d1e6dbcf6b338f0713d45db593802b5f0018
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-12 12:26:16 +0400

    BookSaveService

---
+++ src/main/java/com/mycompany/kitabonline/service/BookSaveService.java
@@ -0,0 +1,39 @@
+package com.mycompany.kitabonline.service;
+
+import com.mycompany.kitabonline.dto.BookListItemResponse;
+import com.mycompany.kitabonline.dto.SaveBookRequest;
+import com.mycompany.kitabonline.entity.Book;
+import com.mycompany.kitabonline.entity.Category;
+import com.mycompany.kitabonline.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookSaveService {
+    private final BookRepository bookRepository;
+    private final CategoryService categoryService;
+
+    public BookListItemResponse saveBook(SaveBookRequest saveBookRequest) {
+        Category category = categoryService.loadCategory(saveBookRequest.getCategoryId());
+        final Book book = Book.builder()
+                .category(category)
+                .title(saveBookRequest.getTitle())
+                .authorName(saveBookRequest.getAuthorName())
+                .bookStatus(saveBookRequest.getBookStatus())
+                .lastPageNumber(saveBookRequest.getLastPageNumber())
+                .totalPage(saveBookRequest.getTotalPage())
+                .build();
+        final Book bookFromDB = bookRepository.save(book);
+        return BookListItemResponse.builder()
+                .categoryName(book.getCategory().getName())
+                .id(bookFromDB.getId())
+                .bookStatus(bookFromDB.getBookStatus())
+                .publisher(bookFromDB.getPublisher())
+                .authorName(bookFromDB.getAuthorName())
+                .totalPage(bookFromDB.getTotalPage())
+                .lastPageNumber(bookFromDB.getLastPageNumber())
+                .build();
+    }
+
+}
