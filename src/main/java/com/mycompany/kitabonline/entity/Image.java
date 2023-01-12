725745ec04a4a3b4464c432661aaa3af10f8cc0a
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-12 12:25:41 +0400

    Image entity

---
+++ src/main/java/com/mycompany/kitabonline/entity/Image.java
@@ -0,0 +1,18 @@
+package com.mycompany.kitabonline.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "image")
+@Getter
+@Builder
+@NoArgsConstructor
+public class Image extends BaseEntity {
+    private Image imageUrl;
+
+}
