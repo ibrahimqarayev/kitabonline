5a11e7a702365afa5296e8bbe9ea7742279ae83f
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-13 18:00:49 +0400

    SaveBookRequest

--- src/main/java/com/mycompany/kitabonline/dto/SaveBookRequest.java
+++ src/main/java/com/mycompany/kitabonline/dto/SaveBookRequest.java
@@ -3,18 +3,26 @@ package com.mycompany.kitabonline.dto;
 import com.mycompany.kitabonline.entity.BookStatus;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.File;
 
 @Data
 public final class SaveBookRequest {
-    private Long bookId;
+    @NotBlank
     private String title;
+    @NotBlank
     private String authorName;
+    @NotNull
     private BookStatus bookStatus;
+    @NotBlank
     private String publisher;
+    @NotNull
     private Integer lastPageNumber;
+    @NotNull
     private Integer totalPage;
     private File image;
+    @NotNull
     private Long categoryId;
 
 
