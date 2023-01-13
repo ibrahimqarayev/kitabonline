62dace76137c695b228cbf956f391634758c4571
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-13 18:03:15 +0400

    CategoryType

--- src/main/java/com/mycompany/kitabonline/dto/CategoryType.java
+++ src/main/java/com/mycompany/kitabonline/dto/CategoryType.java
@@ -1,4 +1,31 @@
 package com.mycompany.kitabonline.dto;
 
 public enum CategoryType {
+    LITERATURE("Library"),
+    COMIC("Comic"),
+    CHILD_AND_YOUTH("Child and youth"),
+    LESSON_TEST_BOOKS("Lesson test books"),
+    EDUCATION_REFERENCE("Education reference"),
+    PHILOSOPHY("Philosophy"),
+    FOREiGN_LANGUAGE("Foreign language"),
+    HOBBY("Hobby"),
+    MYTH_LEGEND("Mth legend"),
+    HUMOR("Humor"),
+    PRESTIGE_BOOKS("Prestige books"),
+    ART_DESIGN("Art design"),
+    AUDIO_BOOKS("Audio books"),
+    OTHER("Other"),
+    SCIENCE("Science"),
+    RESEARCH_HISTORY("Research history"),
+    RELIGION("Religion");
+
+    private final String value;
+
+    CategoryType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
