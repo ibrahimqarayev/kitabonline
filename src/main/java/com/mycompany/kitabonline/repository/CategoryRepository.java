5009ac2967183cd149a9a9d0e969311e2fcb6c07
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-14 20:27:15 +0400

    CategoryRepository

--- src/main/java/com/mycompany/kitabonline/repository/CategoryRepository.java
+++ src/main/java/com/mycompany/kitabonline/repository/CategoryRepository.java
@@ -4,5 +4,8 @@ import com.mycompany.kitabonline.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long>, JpaSpecificationExecutor<Category> {
+    Optional<Category> findByName(String name);
 }
