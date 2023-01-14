fdee59837e696c34ec210b10a7aa49aa435045c1
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-14 20:25:38 +0400

    BookRepository

--- src/main/java/com/mycompany/kitabonline/repository/BookRepository.java
+++ src/main/java/com/mycompany/kitabonline/repository/BookRepository.java
@@ -1,6 +1,5 @@
 package com.mycompany.kitabonline.repository;
 
-import com.mycompany.kitabonline.dto.BookResponse;
 import com.mycompany.kitabonline.entity.Book;
 import com.mycompany.kitabonline.entity.BookStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +8,7 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
-    List<BookResponse> findByBookStatus(BookStatus bookStatus);
+    List<Book> findByBookStatus(BookStatus bookStatus);
+
+    List<Book> findByTitle(String title);
 }
