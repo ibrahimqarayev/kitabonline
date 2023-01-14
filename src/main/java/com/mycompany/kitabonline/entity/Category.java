fa1b4d21e3db71e4f3de46a6261a2ee68a914f6a
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-14 20:25:19 +0400

    Category

--- src/main/java/com/mycompany/kitabonline/entity/Category.java
+++ src/main/java/com/mycompany/kitabonline/entity/Category.java
@@ -1,6 +1,7 @@
 package com.mycompany.kitabonline.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import java.util.List;
 @Entity
 @Table(name = "category")
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Data
 public class Category extends BaseEntity {
