7994bb8a1635d392c671754dc029e6db3499453b
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-14 20:24:51 +0400

    Image

--- src/main/java/com/mycompany/kitabonline/entity/Image.java
+++ src/main/java/com/mycompany/kitabonline/entity/Image.java
@@ -1,5 +1,6 @@
 package com.mycompany.kitabonline.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import javax.persistence.Table;
 @Table(name = "image")
 @Getter
 @Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class Image extends BaseEntity {
     private String imageUrl;
