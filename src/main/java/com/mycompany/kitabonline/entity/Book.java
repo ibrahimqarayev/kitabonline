9e00c02fbfbe3795c25cbfb0004eb0d30a5f306e
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-14 20:26:06 +0400

    Book

--- src/main/java/com/mycompany/kitabonline/entity/Book.java
+++ src/main/java/com/mycompany/kitabonline/entity/Book.java
@@ -1,5 +1,6 @@
 package com.mycompany.kitabonline.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Builder
+@AllArgsConstructor
 public class Book extends BaseEntity {
 
     private String title;
