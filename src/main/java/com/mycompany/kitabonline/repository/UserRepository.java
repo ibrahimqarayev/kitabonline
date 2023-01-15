87d69ee769d0e0a29c3799044c5307563183c25b
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-15 20:20:06 +0400

    UserRepository

--- src/main/java/com/mycompany/kitabonline/repository/UserRepository.java
+++ src/main/java/com/mycompany/kitabonline/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.mycompany.kitabonline.repository;
 
-import com.mycompany.kitabonline.dto.User;
+import com.mycompany.kitabonline.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
