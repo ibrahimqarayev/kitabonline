830a02ec73c0c25b4918d2d5f89acca59786ff53
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-16 10:27:37 +0400

    User

--- src/main/java/com/mycompany/kitabonline/entity/User.java
+++ src/main/java/com/mycompany/kitabonline/entity/User.java
@@ -1,9 +1,12 @@
 package com.mycompany.kitabonline.entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "user")
+@Data
 public class User extends BaseEntity {
 
     @Column(unique = true)
