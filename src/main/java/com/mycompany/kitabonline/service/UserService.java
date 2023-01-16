b9472077062d3473d644f48f61f22b4648eb0729
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-16 10:28:25 +0400

    UserService

---
+++ src/main/java/com/mycompany/kitabonline/service/UserService.java
@@ -0,0 +1,38 @@
+package com.mycompany.kitabonline.service;
+
+import com.mycompany.kitabonline.dto.UserDto;
+import com.mycompany.kitabonline.entity.User;
+import com.mycompany.kitabonline.exception.GenericException;
+import com.mycompany.kitabonline.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserDto create(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        User savedUser = userRepository.save(user);
+
+        return UserDto.builder()
+                .username(savedUser.getUsername())
+                .email(savedUser.getEmail())
+                .role(savedUser.getRole())
+                .build();
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElseThrow(()->GenericException.builder()
+                .httpStatus(HttpStatus.NOT_FOUND)
+                .errorMessage("User not found by given id !")
+                .build());
+    }
+
+}
