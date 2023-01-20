d97cf6cb79f715f605508527770a9ba68ce12dcb
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-20 22:43:01 +0400

    JwtFilter

---
+++ src/main/java/com/mycompany/kitabonline/security/JwtFilter.java
@@ -0,0 +1,26 @@
+package com.mycompany.kitabonline.security;
+
+import com.mycompany.kitabonline.service.UserDetailsServiceImpl;
+import com.mycompany.kitabonline.util.TokenGenerator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final TokenGenerator tokenGenerator;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
