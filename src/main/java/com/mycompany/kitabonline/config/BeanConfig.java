fcafe30dacefab9b3dec1c828b2b8f4ea99487f0
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-22 22:40:13 +0400

    ObjectMapper added

--- src/main/java/com/mycompany/kitabonline/config/BeanConfig.java
+++ src/main/java/com/mycompany/kitabonline/config/BeanConfig.java
@@ -1,5 +1,6 @@
 package com.mycompany.kitabonline.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,4 +13,8 @@ public class BeanConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public ObjectMapper mapper() {
+        return new ObjectMapper();
+    }
 }
