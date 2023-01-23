cdbd80208efb15cdb746c9a3a862057c424c34a5
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-23 14:18:02 +0400

    AuthController

--- src/main/java/com/mycompany/kitabonline/api/AuthController.java
+++ src/main/java/com/mycompany/kitabonline/api/AuthController.java
@@ -1,7 +1,9 @@
 package com.mycompany.kitabonline.api;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
 }
