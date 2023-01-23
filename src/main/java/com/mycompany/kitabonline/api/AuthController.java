c40ace5f70a61338091345cf6eb1cfd655123617
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-23 14:23:01 +0400

    AuthController

--- src/main/java/com/mycompany/kitabonline/api/AuthController.java
+++ src/main/java/com/mycompany/kitabonline/api/AuthController.java
@@ -1,9 +1,18 @@
 package com.mycompany.kitabonline.api;
 
+import com.mycompany.kitabonline.dto.TokenResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponseDto> login() {
+
+    }
+
 }
