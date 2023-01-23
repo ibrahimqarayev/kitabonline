26216b13d5dd606c3b2ceee29d218f83cdb180a7
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-23 15:00:16 +0400

    AuthController

--- src/main/java/com/mycompany/kitabonline/api/AuthController.java
+++ src/main/java/com/mycompany/kitabonline/api/AuthController.java
@@ -1,18 +1,25 @@
 package com.mycompany.kitabonline.api;
 
 import com.mycompany.kitabonline.dto.TokenResponseDto;
+import com.mycompany.kitabonline.request.LoginRequest;
+import com.mycompany.kitabonline.service.AuthService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
-    @PostMapping("/login")
-    public ResponseEntity<TokenResponseDto> login() {
+    private final AuthService authService;
 
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponseDto> login(@RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok(authService.login(loginRequest));
     }
 
 }
