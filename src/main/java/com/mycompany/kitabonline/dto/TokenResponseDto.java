b209d8214f4884a1d25ce3b22984700f8d4897bf
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-23 14:18:28 +0400

    TokenResponseDto

---
+++ src/main/java/com/mycompany/kitabonline/dto/TokenResponseDto.java
@@ -0,0 +1,13 @@
+package com.mycompany.kitabonline.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponseDto {
+}
