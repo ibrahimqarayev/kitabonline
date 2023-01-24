0f716da5f3168eaeb594b6af32c1a6fab63a82f7
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-24 23:39:50 +0400

    StartupConfig

---
+++ src/main/java/com/mycompany/kitabonline/config/StartupConfig.java
@@ -0,0 +1,27 @@
+package com.mycompany.kitabonline.config;
+
+import com.mycompany.kitabonline.entity.Role;
+import com.mycompany.kitabonline.entity.User;
+import com.mycompany.kitabonline.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class StartupConfig implements CommandLineRunner {
+
+    private final UserService userService;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userService.createUser(User.builder()
+                .username("Ibrahim")
+                .password("12345")
+                .email("qarayevibrahimm@gmail.com")
+                .role(Role.ADMIN)
+                .build());
+
+    }
+}
