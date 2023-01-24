94d5da4cc435e2d1d30566d2842f3f1fad10e25c
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-24 23:11:51 +0400

    AuthService

--- src/main/java/com/mycompany/kitabonline/service/AuthService.java
+++ src/main/java/com/mycompany/kitabonline/service/AuthService.java
@@ -1,10 +1,14 @@
 package com.mycompany.kitabonline.service;
 
 import com.mycompany.kitabonline.dto.TokenResponseDto;
+import com.mycompany.kitabonline.exception.GenericException;
 import com.mycompany.kitabonline.request.LoginRequest;
 import com.mycompany.kitabonline.util.TokenGenerator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,5 +20,21 @@ public class AuthService {
     private final AuthenticationManager authenticationManager;
 
     public TokenResponseDto login(LoginRequest loginRequest) {
+
+        try {
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+            return TokenResponseDto.builder()
+                    .accessToken(tokenGenerator.generateToken(auth))
+                    .userDto(userService.getUser(loginRequest.getUsername()))
+                    .build();
+        }catch (Exception exception){
+            throw GenericException.builder()
+                    .httpStatus(HttpStatus.NOT_FOUND)
+                    .errorMessage("User not found !")
+                    .build();
+        }
+
     }
 }
