02bc3c7b3040596f17e9f8a68c1d837d9ef507ca
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-24 22:53:04 +0400

    AuthService

--- src/main/java/com/mycompany/kitabonline/service/AuthService.java
+++ src/main/java/com/mycompany/kitabonline/service/AuthService.java
@@ -1,7 +1,20 @@
 package com.mycompany.kitabonline.service;
 
+import com.mycompany.kitabonline.dto.TokenResponseDto;
+import com.mycompany.kitabonline.request.LoginRequest;
+import com.mycompany.kitabonline.util.TokenGenerator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final UserService userService;
+    private final TokenGenerator tokenGenerator;
+    private final AuthenticationManager authenticationManager;
+
+    public TokenResponseDto login(LoginRequest loginRequest) {
+    }
 }
