00e08ac88f4805f31526f74c13f01881f9f30824
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-24 23:08:55 +0400

    TokenResponseDto

--- src/main/java/com/mycompany/kitabonline/dto/TokenResponseDto.java
+++ src/main/java/com/mycompany/kitabonline/dto/TokenResponseDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TokenResponseDto {
-    private String token;
+    private String accessToken;
     private UserDto userDto;
 
 }
