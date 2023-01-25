25f0b5af91a158eabb24dce8535e76b4ff55d3e6
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-25 22:43:48 +0400

    TestEndpoint

--- src/main/java/com/mycompany/kitabonline/api/TestEndpoint.java
+++ src/main/java/com/mycompany/kitabonline/api/TestEndpoint.java
@@ -1,9 +1,17 @@
 package com.mycompany.kitabonline.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class TestEndpoint {
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+
 }
