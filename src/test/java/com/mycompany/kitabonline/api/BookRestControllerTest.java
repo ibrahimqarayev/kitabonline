af0a15356695416fc1afdc2337d276fcaebb3123
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-27 23:39:56 +0400

    BookRestControllerTest

--- src/test/java/com/mycompany/kitabonline/api/BookRestControllerTest.java
+++ src/test/java/com/mycompany/kitabonline/api/BookRestControllerTest.java
@@ -1,7 +1,23 @@
 package com.mycompany.kitabonline.api;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+import com.mycompany.kitabonline.service.BookListService;
+import com.mycompany.kitabonline.service.BookSaveService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class BookRestControllerTest {
 
+    @Mock
+    private BookListService bookListService;
+    @Mock
+    private BookSaveService bookSaveService;
+
+    @Test
+    void itShouldSaveBook_WhenValidBookRequestBody() {
+    }
 }
