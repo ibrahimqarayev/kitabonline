6b042450a3b328639c72938d1e5d43cef459a3c3
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-27 23:41:47 +0400

    BookRestControllerTest

--- src/test/java/com/mycompany/kitabonline/api/BookRestControllerTest.java
+++ src/test/java/com/mycompany/kitabonline/api/BookRestControllerTest.java
@@ -4,9 +4,11 @@ package com.mycompany.kitabonline.api;
 import com.mycompany.kitabonline.service.BookListService;
 import com.mycompany.kitabonline.service.BookSaveService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -16,6 +18,9 @@ class BookRestControllerTest {
     private BookListService bookListService;
     @MockBean
     private BookSaveService bookSaveService;
+
+    @Autowired
+    MockMvc mockMvc;
 
     @Test
     void itShouldSaveBook_WhenValidBookRequestBody() {
